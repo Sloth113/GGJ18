db4501b813820f6c2478d1eb5e441e2cdb45309c
TK <antoine.kenneth.odi@gmail.com>
2018-01-26 22:57:57 +1030

    Enemy AI initial test

---
+++ Assets/Test/Scripts/RobotNavigation.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RobotNavigation : MonoBehaviour {
+
+    //[SerializeField] private Vector3 destination;
+    [SerializeField] private int m_speed = 1;
+    [SerializeField] private int m_maxHealth = 1;
+    [SerializeField] private int m_power = 1;
+
+    private NavMeshAgent m_agent;
+    private int m_health;
+
+    public GameObject endDestination;
+
+
+    // Use this for initialization
+    void Start () {
+        m_agent = GetComponent<NavMeshAgent>();
+        m_health = m_maxHealth;
+        m_agent.speed = m_speed;
+        if (endDestination != null)
+            m_agent.destination = endDestination.transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (endDestination != null)
+            m_agent.destination = endDestination.transform.position;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject == endDestination)
+        {
+            // Destroy itself
+            // Game manager call or tower call
+        }
+    }
+
+    void OnHit(int damage)
+    {
+        m_health -= damage;
+        if (m_health <= 0)
+            OnDeath();
+    }
+
+    void OnDeath ()
+    {
+        // gameManager.addPower(power);
+        Destroy(gameObject);
+        // Destroy gameObject
+        // Explosion particle system
+        // Power gain particle system
+    }
+}
