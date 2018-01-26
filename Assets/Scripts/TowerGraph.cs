3e5f8f027570a542fd5f1f5664a2ef71299e9fe1
AIE\student <thrawn369@gmail.com>
2018-01-26 22:38:17 +1030

    Created base Tower class and functions for creating acyclic graph for power supply and calculating power sent to towers

---
+++ Assets/Scripts/TowerGraph.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerGraph  {
+
+    public PowerSource source;
+
+    public List<Tower> towers;
+
+
+    public void CalculatePowerGraph()
+    {
+        foreach (Tower tower in towers)
+        {
+            tower.visited = false;
+            tower.inStack = false;
+        }
+        source.CalculateChildren();
+    }
+
+    public void SupplyPower(float power)
+    {
+        foreach (Tower tower in towers)
+        {
+            tower.ResetPower();
+        }
+        source.AddPower(power);
+    }
+}
