86d0a04c026565b8ba97900ff23ce99791fdedd4
AIE\student <thrawn369@gmail.com>
2018-01-27 08:51:19 +1030

    Gun tower now shoots laser

--- Assets/Scripts/Towers/GunTower.cs
+++ Assets/Scripts/Towers/GunTower.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class GunTower : AttackTower {
 
+    public LineRenderer laser;
+
+    [SerializeField] float laserDisplayTime = 0.2f;
+    [SerializeField] float laserTimer = 0.0f;
+    [SerializeField] bool laserShowing = false;
+
     // Use this for initialization
     protected override void Start () {
         base.Start();
@@ -12,6 +18,16 @@ public class GunTower : AttackTower {
 	// Update is called once per frame
 	protected override void Update () {
         base.Update();
+        if (laserShowing)
+        {
+            laserTimer += Time.deltaTime;
+            if(laserTimer >= laserDisplayTime)
+            {
+                laserShowing = false;
+                laserTimer = 0;
+                laser.gameObject.SetActive(false);
+            }
+        }
 	}
 
     protected override void ShootEnemy(RobotNavigation enemy)
@@ -19,5 +35,16 @@ public class GunTower : AttackTower {
         shotReady = false;
         reloadProgress = 0;
         Debug.Log("Shots fired");
+        ShowLaser(enemy.gameObject.transform.position);
+        //TODO play shooting noise
+    }
+
+    private void ShowLaser(Vector3 target)
+    {
+        laser.SetPosition(0, transform.position);
+        laser.SetPosition(1, target);
+        laserTimer = 0;
+        laserShowing = true;
+        laser.gameObject.SetActive(true);
     }
 }
