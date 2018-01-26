d18df22873b0ef32dd02ce1420ba8a7260baf60f
TK <antoine.kenneth.odi@gmail.com>
2018-01-27 04:00:12 +1030

    Added tower placement code

--- Assets/Test/Scripts/TowerPlacement.cs
+++ Assets/Test/Scripts/TowerPlacement.cs
@@ -1,22 +1,52 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-//public class TowerPlacement : MonoBehaviour
-//{
+public class TowerPlacement : MonoBehaviour
+{
 
-//    [SerializeField]
-//    List<GameObject>();
+    [SerializeField] private List<GameObject> towerPrefabs;
+    [SerializeField] private Transform groundTransform;
 
-//	// Use this for initialization
-//	void Start()
-//    {
+    private bool towerNotPlaced = false;
 
-//    }
+	// Use this for initialization
+	void Start()
+    {
 
-//    // Update is called once per frame
-//    void Update()
-//    {
+    }
 
-//    }
-//}
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Keypad1) && towerPrefabs[0] != null)
+        {
+            towerNotPlaced = true;
+        }
+
+        if (towerNotPlaced == true)
+        {
+            Vector3 mouseCast = towerAtMousePosition();
+            if (Input.GetMouseButtonDown(0))
+            {
+                Instantiate(towerPrefabs[0], mouseCast, Quaternion.identity);
+                towerNotPlaced = false;
+            }
+        }
+    }
+
+    private Vector3 towerAtMousePosition()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Ray mouseRay = Camera.main.ScreenPointToRay(mousePos);
+        Plane groundPlane = new Plane(Vector3.up, groundTransform.position);
+
+        float rayDistance = 0;
+
+        groundPlane.Raycast(mouseRay, out rayDistance);
+        Vector3 castPoint = mouseRay.GetPoint(rayDistance);
+
+        return castPoint;
+    }
+
+}
