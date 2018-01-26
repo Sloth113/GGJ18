2a93b1767ff0172eb0cbcac973ec5d24cce42228
TK <antoine.kenneth.odi@gmail.com>
2018-01-27 03:11:32 +1030

    Finalise Enemy art assets

---
+++ Assets/Test/Scripts/RobotRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotRotation : MonoBehaviour {
+
+    private Vector3 transformAngle;
+
+	// Use this for initialization
+	void Start () {
+        transformAngle = transform.eulerAngles;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transformAngle = transform.eulerAngles;
+
+        transformAngle.y += Mathf.Sin(Time.time) * Mathf.Rad2Deg;
+        transform.eulerAngles = transformAngle;
+	}
+}
