4b6f7ff1c801f7d23abab38218c4dda022772cd3
AIE\student <thrawn369@gmail.com>
2018-01-28 01:53:21 +1030

    added secret scene to build list, put caw animation event in

---
+++ Assets/Scripts/chicken.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chicken : MonoBehaviour {
+
+    public PlaySoundV2 audio;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Caw()
+    {
+        audio.PlayV2(0);
+    }
+}
