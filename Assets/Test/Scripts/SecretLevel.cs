75e67c2845758ee77546cd16348e477a6eac54a6
AIE\student <thrawn369@gmail.com>
2018-01-27 22:42:03 +1030

    no message

--- Assets/Test/Scripts/SecretLevel.cs
+++ Assets/Test/Scripts/SecretLevel.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class SecretLevel : MonoBehaviour {
 
+    public PowerSource source;
+    public JunctionTower splitter;
+    public AOETower aoe;
+    public GunTower gun;
+
+    bool started = false;
+
 	// Use this for initialization
 	void Start () {
-		
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (!started)
+        {
+            splitter.MakeConnection(aoe);
+            splitter.MakeConnection(gun);
+            source.CalculateChildren();
+            source.AddPower(60);
+            started = true;
+        }
         if (Input.GetButtonDown("Cancel"))
         {
             GameManager.Instance.unxyxxy();
