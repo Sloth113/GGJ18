a446d589c29f79972297570772efb887bdf8f3cf
Bax <voop.sd@gmail.com>
2018-03-08 16:20:20 +1030

    Added Tutorial and Credits (still needs to be put together)

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour {
+
+    public List<Sprite> tutorialImages;
+    public Image imageOutput;
+    [TextArea]
+    public List<string> tutorialText;
+
+    public Text textOutput;
+    public GameObject pageForward;
+    public GameObject pageBack;
+
+
+    int currentPage = 0;
+
+    void Start()
+    {
+        imageOutput.sprite = tutorialImages[currentPage];
+        textOutput.text = tutorialText[currentPage];
+        if (currentPage >= tutorialImages.Count - 1)
+        {
+            pageForward.SetActive(false);
+        }
+        else
+        {
+            pageBack.SetActive(true);
+        }
+        if (currentPage <= 0)
+        {
+            pageBack.SetActive(false);
+        }
+        else
+        {
+            pageForward.SetActive(true);
+        }
+    }
+
+
+    public void PageForward() {
+        currentPage += 1;
+        imageOutput.sprite = tutorialImages[currentPage];
+        textOutput.text = tutorialText[currentPage];
+        if (currentPage >= tutorialImages.Count - 1)
+        {
+            pageForward.SetActive(false);
+        }
+        else
+        {
+            pageBack.SetActive(true);
+        }
+    }
+
+    public void PageBack()
+    {
+        currentPage -= 1;
+        imageOutput.sprite = tutorialImages[currentPage];
+        textOutput.text = tutorialText[currentPage];
+        if (currentPage <= 0)
+        {
+            pageBack.SetActive(false);
+        }
+        else
+        {
+            pageForward.SetActive(true);
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
